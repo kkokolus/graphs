d01800f12ce54ad9ef58a175ab81bae833ddd0e1
Kornelia Kokolus <corella@op.pl>
2016-10-15 13:08:06 +0200

    Initial commit

---
+++ Graph_WF/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Graph_WF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Dane = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 81);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 581);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.panel1);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(984, 662);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.Dane);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(981, 72);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(110, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(287, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Dane
+            // 
+            this.Dane.AutoSize = true;
+            this.Dane.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
+            this.Dane.Location = new System.Drawing.Point(33, 27);
+            this.Dane.Name = "Dane";
+            this.Dane.Size = new System.Drawing.Size(58, 22);
+            this.Dane.TabIndex = 1;
+            this.Dane.Text = "Dane:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 662);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label Dane;
+        private System.Windows.Forms.TextBox textBox1;
+
+    }
+}
+
---
+++ Graph_WF/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graph_WF
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Width = 400;
+            
+        }
+        
+        public void Rysuj()
+        {
+            /*Point p1 = new Point(50, 50);
+            Point p2 = new Point(10, 10);*/
+            Pen pen = new Pen(Color.Green);
+            Graphics g = pictureBox1.CreateGraphics();
+            //g.DrawLine(pen, p1, p2);
+           // g.DrawEllipse(pen, 100, 50, 180, 180);
+
+            Random r = new Random();
+            int ile = 5;
+
+            Point [] points = new Point[ile];
+
+            for (int i = 0; i < ile; i++)
+            {
+                g.Clear(Form1.DefaultBackColor);
+                int alpha = r.Next(-180, 180);
+                
+                points[i] = new Point((int)((pictureBox1.Width / 2) + 100 * Math.Cos(alpha)),
+                                      (int)((pictureBox1.Height / 2) + 100 * Math.Sin(alpha)));
+
+                int previous_alpha = alpha;
+                if (previous_alpha - alpha < 30)
+                {
+                    alpha = r.Next(-180, 180);
+                }
+			}
+
+            for (int i = 0; i < ile-1; i++)
+            {
+                
+                g.DrawLine(pen, points[i], points[i+1]);
+            }
+
+            
+
+          /*  int alpha = r.Next(-180, 180);
+            Point p1 = new Point((int)(50 + 20 * Math.Cos(alpha)),
+                                 (int)(50 + 20 * Math.Cos(alpha)));
+
+            int beta = r.Next(-180, 180);
+
+            Point p2 = new Point((int)(50 + 20 * Math.Cos(beta)),
+                                 (int)(50 + 20 * Math.Cos(beta)));*/
+
+            //g.DrawLine(pen, p1, p2);
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Rysuj();
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}

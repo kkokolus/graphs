cfe97568a23e3806aee77cf634fce3ea7ab4f66a
Kornelia Kokolus <corella@op.pl>
2016-10-16 18:52:45 +0200

    Rewrited program

--- Graph_WF/Form1.Designer.cs
+++ Graph_WF/Form1.Designer.cs
@@ -28,25 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.visualization = new System.Windows.Forms.PictureBox();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.vertexes = new System.Windows.Forms.TextBox();
+            this.dane = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.Dane = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.visualization)).BeginInit();
             this.flowLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // visualization
             // 
-            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 81);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(400, 581);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.visualization.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.visualization.Location = new System.Drawing.Point(0, 81);
+            this.visualization.Name = "visualization";
+            this.visualization.Size = new System.Drawing.Size(400, 581);
+            this.visualization.TabIndex = 0;
+            this.visualization.TabStop = false;
+            this.visualization.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // flowLayoutPanel1
             // 
@@ -59,41 +60,50 @@
             // panel1
             // 
             this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Controls.Add(this.Dane);
+            this.panel1.Controls.Add(this.vertexes);
+            this.panel1.Controls.Add(this.dane);
             this.panel1.Location = new System.Drawing.Point(3, 3);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(981, 72);
             this.panel1.TabIndex = 0;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.visualization_Paint);
+            // 
+            // vertexes
+            // 
+            this.vertexes.Location = new System.Drawing.Point(110, 27);
+            this.vertexes.Name = "vertexes";
+            this.vertexes.Size = new System.Drawing.Size(287, 20);
+            this.vertexes.TabIndex = 2;
+            this.vertexes.TextChanged += new System.EventHandler(this.vertexes_TextChanged);
+            // 
+            // dane
+            // 
+            this.dane.AutoSize = true;
+            this.dane.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
+            this.dane.Location = new System.Drawing.Point(33, 27);
+            this.dane.Name = "dane";
+            this.dane.Size = new System.Drawing.Size(58, 22);
+            this.dane.TabIndex = 1;
+            this.dane.Text = "Dane:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(110, 27);
+            this.textBox1.Location = new System.Drawing.Point(456, 28);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(287, 20);
-            this.textBox1.TabIndex = 2;
-            // 
-            // Dane
-            // 
-            this.Dane.AutoSize = true;
-            this.Dane.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
-            this.Dane.Location = new System.Drawing.Point(33, 27);
-            this.Dane.Name = "Dane";
-            this.Dane.Size = new System.Drawing.Size(58, 22);
-            this.Dane.TabIndex = 1;
-            this.Dane.Text = "Dane:";
+            this.textBox1.Size = new System.Drawing.Size(197, 20);
+            this.textBox1.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 662);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.visualization);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.visualization)).EndInit();
             this.flowLayoutPanel1.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
@@ -103,12 +113,12 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox visualization;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label Dane;
+        private System.Windows.Forms.Label dane;
+        private System.Windows.Forms.TextBox vertexes;
         private System.Windows.Forms.TextBox textBox1;
-
     }
 }
 
--- Graph_WF/Form1.cs
+++ Graph_WF/Form1.cs
@@ -7,67 +7,155 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace Graph_WF
 {
     public partial class Form1 : Form
     {
+        
+
 
         public Form1()
         {
             InitializeComponent();
-            pictureBox1.Width = 400;
+            visualization.Width = 400;
+            Punkty();
+        }
+
+        string[] num_of_vert;
+        int alpha;
+        Random r = new Random();
+        // Generowanie punktow na okregu
+        Dictionary<Point, int> points = new Dictionary<Point, int>();
+
+        public void Punkty()
+        {
+            /* Point p1 = new Point(50, 50);
+            Point p2 = new Point(10, 10); */
+            //Pen pen = new Pen(Color.Green);
+            // Graphics g = visualization.CreateGraphics();
+            // g.DrawLine(pen, p1, p2);
+            // g.DrawEllipse(pen, 100, 50, 180, 180);
+
+            
+           // int ile = 5;
+
+
+            //NOWE
+           
+
+            //dziala na enter
+            vertexes.KeyUp += TextBoxKeyUp;
+            // to sie teraz wykonalo string[] num_of_vert = Regex.Split(vertexes.Text, ", ");
+
+
+
             
         }
-        
-        public void Rysuj()
-        {
-            /*Point p1 = new Point(50, 50);
-            Point p2 = new Point(10, 10);*/
-            Pen pen = new Pen(Color.Green);
-            Graphics g = pictureBox1.CreateGraphics();
-            //g.DrawLine(pen, p1, p2);
-           // g.DrawEllipse(pen, 100, 50, 180, 180);
 
-            Random r = new Random();
-            int ile = 5;
 
-            Point [] points = new Point[ile];
+           /* points.Add(Regex.Split(vertexes.Text, ", ");
+
+            
+           
+
+            
+
+
+            
 
             for (int i = 0; i < ile; i++)
             {
                 g.Clear(Form1.DefaultBackColor);
-                int alpha = r.Next(-180, 180);
-                
-                points[i] = new Point((int)((pictureBox1.Width / 2) + 100 * Math.Cos(alpha)),
-                                      (int)((pictureBox1.Height / 2) + 100 * Math.Sin(alpha)));
+                alpha = r.Next(-180, 180);
 
-                int previous_alpha = alpha;
-                if (previous_alpha - alpha < 30)
-                {
-                    alpha = r.Next(-180, 180);
-                }
-			}
-
-            for (int i = 0; i < ile-1; i++)
-            {
-                
-                g.DrawLine(pen, points[i], points[i+1]);
+                points[i] = new Point((int)((visualization.Width / 2) + 100 * Math.Cos(alpha)),
+                                      (int)((visualization.Height / 2) + 100 * Math.Sin(alpha)));
             }
 
-            
+            // Odstep miedzy punktami
+            for (int i = 0; i < points.Length - 1; i++)
+            {
+                foreach (Point p in points)
+                {
+                    if (p != points[i] && (Math.Abs(p.X - points[i].X) <= 40 || Math.Abs(p.Y - points[i].Y) <= 40))
+                    {
+                        alpha = r.Next(-180, 180);
+                        points[i] = new Point((int)((visualization.Width / 2) + 100 * Math.Cos(alpha)),
+                                     (int)((visualization.Height / 2) + 100 * Math.Sin(alpha)));
+                    }
+                };
 
-          /*  int alpha = r.Next(-180, 180);
-            Point p1 = new Point((int)(50 + 20 * Math.Cos(alpha)),
-                                 (int)(50 + 20 * Math.Cos(alpha)));
+            }
 
-            int beta = r.Next(-180, 180);
-
-            Point p2 = new Point((int)(50 + 20 * Math.Cos(beta)),
-                                 (int)(50 + 20 * Math.Cos(beta)));*/
-
-            //g.DrawLine(pen, p1, p2);
         }
+            // Rysowanie krawedzi
+
+            void Rysuj()
+            {
+                Pen pen = new Pen(Color.Green);
+                Graphics g = visualization.CreateGraphics();
+
+                
+
+            for (int i = 0; i < ile - 1; i++)
+                {
+                    g.DrawLine(pen, points[i], points[i + 1]);
+                }
+             }*/
+
+
+
+
+
+
+
+        //Generowanie punktu
+        public Point random_point()
+        {
+            alpha = r.Next(-180, 180);
+            return new Point((int)((visualization.Width / 2) + 100 * Math.Cos(alpha)),
+                                   (int)((visualization.Height / 2) + 100 * Math.Sin(alpha)));
+        }
+
+
+
+
+
+        // Nacisniecie entera w textboxie
+        private void TextBoxKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                num_of_vert = Regex.Split(vertexes.Text, ", ");
+                // Rysuj();
+
+                foreach (string v in num_of_vert)
+                {
+                    points.Add(random_point(), Convert.ToInt32(v));
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+        /*  int alpha = r.Next(-180, 180);
+          Point p1 = new Point((int)(50 + 20 * Math.Cos(alpha)),
+                               (int)(50 + 20 * Math.Cos(alpha)));
+
+          int beta = r.Next(-180, 180);
+
+          Point p2 = new Point((int)(50 + 20 * Math.Cos(beta)),
+                               (int)(50 + 20 * Math.Cos(beta)));*/
+
+        //g.DrawLine(pen, p1, p2);
+    
 
 
 
@@ -78,11 +166,16 @@ namespace Graph_WF
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            Rysuj();
+         //   Rysuj();
             
         }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
+        private void visualization_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void vertexes_TextChanged(object sender, EventArgs e)
         {
 
         }
